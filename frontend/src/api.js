9965b7c4ae9dd038a1f3a341c65b75e839de6e0f
RUSLAN <63105241+RUSLANBALTABAEV@users.noreply.github.com>
2025-09-16 15:31:34 +0500

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,5 +0,0 @@
-const api = axios.create({
-  baseURL: "http://localhost:8000", // Для локального запуска без Docker.
-  // Или для Docker:
-  // baseURL: "http://backend:8000",  // Имя сервиса из docker-compose.yml.
-});
